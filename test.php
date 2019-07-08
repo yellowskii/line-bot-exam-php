2e6b41574b578fed3476e161ae03a792fd41c53a
yellowskii <yellowskii666@gmail.com>
2019-07-08 08:54:41 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,30 @@
 <?php
 		echo "WELCOME TO JURASSIC PARK";
+
+		//Get DB
+				$server = "118.172.127.41";
+				$suser = "root";
+				$spassword = "";
+				$database = "ktb-line-bot";
+
+				$conn = mysqli_connect($server,$suser,$spassword,$database);
+
+
+				if (mysqli_connect_errno()) {
+				    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				}
+				
+				$strSQL = "SELECT HN, fname, lname, cid,  appoint_date, clinic, doctor, tel, note 
+						   FROM medapp
+						   JOIN account
+						   ON medapp.HN = account.HN";
+			
+				$result = mysqli_query($conn,$strSQL);
+				while($row = mysqli_fetch_assoc($result)) {
+					$dbtext = "คุณ ".$row["fname"]." ".$row["lname"]." นัดคลินิก ".$row["clinic"]." กับแพทย์ ".$row["doctor"]." คือวันที่ ".$row["appoint_date"];
+				}
+
+
+echo $dbtext;
+
 ?>
