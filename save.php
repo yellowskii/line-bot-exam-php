258ad971f73a927f22d38b431dbbcab77ddcd8a3
yellowskii <yellowskii666@gmail.com>
2019-08-08 16:14:51 +0700

    Add files via upload

--- save.php
+++ save.php
@@ -79,6 +79,7 @@
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/popper.js"></script>
