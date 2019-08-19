be438f1ceb02aecce93c10a6c64fef559440b028
yellowskii <yellowskii666@gmail.com>
2019-08-19 15:47:54 +0700

    Add files via upload

--- botpush.php
+++ botpush.php
@@ -8,19 +8,27 @@ $access_token = 'wFJmyBRjsUu7s8WP2ucTr6KhCMjl0h3Wt8UXcbwRM9IG0sRVs6QXWSzUGQ/WX4H
 
 $channelSecret = '977670d76e802dffac5da90001614136';
 
-$pushID = 'U3b837ec7315a4f85582a4c35c405a2e8';
+
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('แจ้งเตือนนัดการตรวจรักษาของคุณ AA BBB ได้มีการนัดกับหมอ CCC ไวเในวันที้ XX YY ZZZZ ค่ะ');
+
+$pushID = 'U3b837ec7315a4f85582a4c35c405a2e8';
+
+$i=0;
+for($i<3){
+
+if($i==0)
+  $text = "แจ้งเตือนนัดการตรวจรักษาของคุณ AA";
+else if($i==1)
+  $text = "แจ้งเตือนนัดการตรวจรักษาของคุณ BB";
+else
+  $text = "แจ้งเตือนนัดการตรวจรักษาของคุณ CC";
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-
-
-
-
-
-
+}
