625783510ce3d9f3a775b75010cbb2f29f84cc6b
yellowskii <yellowskii666@gmail.com>
2019-08-19 11:27:11 +0700

    Add files via upload

--- test.php
+++ test.php
@@ -6,26 +6,31 @@
 				$suser = "botadmin";
 				$spassword = "ktb5570";
 				$database = "ktb-line-bot";
-			
+
 
 				$conn = mysqli_connect($server,$suser,$spassword,$database);
 
 
-				if(mysqli_connect_error()) 
-					echo "Connection Error. ".mysqli_connect_error(); 
+				if(mysqli_connect_error())
+					echo "Connection Error. ".mysqli_connect_error();
 				    else
-					echo "Database Connection Successfully."; 
-				
+					echo "Database Connection Successfully.";
+
 				$strSQL = "SELECT medapp.HN, medapp.clinic, medapp.doctor, medapp.appoint_date
 						FROM medapp
 						JOIN account
 						ON medapp.HN = account.HN";
-			
-				$result = mysqli_query($conn,$strSQL);
-				while($row = mysqli_fetch_array($result)) {
-					$dbtext = "HN ".$row["0"]." ไ้ด้มีนัดที่ ".$row["1"]." กับหมอ ".$row["2"]." ในวันที่ ".$row["3"];
-				}
 
+				$result = mysqli_query($conn,$strSQL);
+
+				if (mysqli_num_rows($result) > 0) {
+				    // output data of each row
+				    while($row = mysqli_fetch_assoc($result)) {
+
+				    }
+				} else {
+				    echo "0 results";
+				}
 
 echo $dbtext;
 
