3701c13d19b6a163978a060b1f3d004745bb7a8c
yellowskii <yellowskii666@gmail.com>
2019-09-04 15:46:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -112,10 +112,6 @@ if (!is_null($events['events'])) {
 				$messages = [
 						'type' => 'text',
 						'text' => $text
-				],
-				[
-						'type' => 'text',
-						'text' => $text
 				];
 
 			// Make a POST Request to Messaging API to reply to sender
