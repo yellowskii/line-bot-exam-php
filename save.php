9819dcbac240ddae2634ece54d1f9fab075e035f
yellowskii <yellowskii666@gmail.com>
2020-04-15 15:39:55 +0700

    Update save.php

--- save.php
+++ save.php
@@ -71,11 +71,13 @@
 </div>
 
 	<?php
-		} else{
+		} else{		
+				$name = preg_replace("/[^a-zA-Z0-9]+/", "", html_entity_decode($_POST["fname"], ENT_QUOTES));
+				$lname = preg_replace("/[^a-zA-Z0-9]+/", "", html_entity_decode($_POST["lname"], ENT_QUOTES));
 				$profile_name = preg_replace("/[^a-zA-Z0-9]+/", "", html_entity_decode($_POST["uprofile"], ENT_QUOTES));
 
 				$strSQL = "INSERT INTO account (fname, lname, cid, profile,line_id)
-									VALUES ('".$_POST["fname"]."', '".$_POST["lname"]."','".$_POST["cid"]."', '".$profile_name."',  '".$_POST["uid"]."')";
+									VALUES ('".$name."', '".$lname."','".$_POST["cid"]."', '".$profile_name."',  '".$_POST["uid"]."')";
 
 				if(mysqli_query($conn,$strSQL)){
 
