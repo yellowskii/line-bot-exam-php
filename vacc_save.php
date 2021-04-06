1299adb10bfcc47c9071f9623f6d8ba4ca57fb6d
yellowskii <yellowskii666@gmail.com>
2021-04-06 22:21:57 +0700

    Add files via upload

--- vacc_save.php
+++ vacc_save.php
@@ -35,9 +35,11 @@
 	<?php
 		} else{
 
+				$date= date("Y-m-d");
+
 				$strSQL = "INSERT INTO vaccine_app (target, province, city, sub, moo, station, code, title,name,surname,gender,birthdate,cid,,tel,vacc,appoint_date,input_date,profile,userID)
 									VALUES ('5-ประชาชนทั่วไป', '74-สมุทรสาคร','02-กระทุ่มแบน', '".$_POST["thambon"]."', '".$_POST["moo"]."', 'โรงพยาบาลกระทุ่มแบน', '11304', '".$_POST["prename"]."',
-										 				'".$_POST["name"]."', '".$_POST["surname"]."','".$_POST["gender"]."', '".$_POST["bday"]."', '".$_POST["cid"]."', '".$_POST["tel]."',)";
+										 				'".$_POST["name"]."', '".$_POST["surname"]."','".$_POST["gender"]."', '".$_POST["bday"]."', '".$_POST["cid"]."', '".$_POST["tel"]."',".1.", '".$_POST["appoint_date"]."', '".$date."','".$_POST["uprofile"]."', '".$_POST["uid"]."',)";
 
 				if(mysqli_query($conn,$strSQL)){
 
