7aeb5a3e6aa5aa1fce6cbc8d28f6fd947fd4fd54
yellowskii <yellowskii666@gmail.com>
2021-04-07 13:29:53 +0700

    Add files via upload

--- vacc_register.php
+++ vacc_register.php
@@ -123,7 +123,7 @@
 <div class="input-group">
 <label  class="col-2" for="bday">วันเกิด</label>
 <div class="col">
- <input type="date" class="form-control mb-2" name="bday" id="bday" min="1941-01-01" max="2004-01-01">
+ <input type="date" class="form-control mb-2" name="bday" id="bday" min="1941-01-01" max="2004-01-01" required>
 </div>
 </div>
 
@@ -220,8 +220,6 @@
   	});
 
       });
-
-
 
   };
 
