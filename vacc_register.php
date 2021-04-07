6a7fd410491dd2322535e2c14878cb1740ab201d
yellowskii <yellowskii666@gmail.com>
2021-04-07 16:05:30 +0700

    Add files via upload

--- vacc_register.php
+++ vacc_register.php
@@ -124,7 +124,7 @@
 <div class="input-group">
 <label  class="col-2" for="bday">วันเกิด</label>
 <div class="col">
- <input type="date" class="form-control mb-2" name="bday" id="bday" required>
+ <input type="text" class="form-control mb-2" name="bday" id="bday" required>
 </div>
 </div>
 
@@ -227,10 +227,10 @@
   					liff.closeWindow();
   	});
       });
-
+				$('#bday').calendarsPicker({calendar: $.calendars.instance('thai','th')});
   };
 
-	$('#bday').calendarsPicker({calendar: $.calendars.instance('thai','th')});
+
 /*
   function ageCount() {
     var now =new Date();                            //getting current date
