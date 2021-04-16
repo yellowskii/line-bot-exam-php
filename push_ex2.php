16d5b5dbebcb9ec7ce42238a42230b25cda265bd
yellowskii <yellowskii666@gmail.com>
2021-04-16 18:15:47 +0700

    Update push_ex2.php

--- push_ex2.php
+++ push_ex2.php
@@ -11,16 +11,43 @@ $channelSecret = '977670d76e802dffac5da90001614136';
 
 
 
+    $server = "voipktbh.dyndns.org";
+    $suser = "botadmin";
+    $spassword = "ktb5570";
+    $database = "ktb-line-bot";
 
 
-    $msg_array = array("สำหรับคุณสมพร คลินิคอายุรกรรมแจ้งให้มายื่นใบนัดและเจาะเลือดช่วงเช้าตามปกติแล้วกลับบ้านได้
-    แล้วให้ญาติอายุน้อยมารับยาและใบนัดที่คลินิคอายุรกรรม ชั้น1 ช่วง 13.00-15.00น.",
-                      "สำหรับคุณไพศาล มีการขาดการติดตามผลเลือดมานาน ตั้งแต่ตค.ปีที่แล้ว 
-                      เห็นควรให้มาตรวจเจาะเลือดเพื่อปรับยาไม่สามารถให้มารับยาเดิมได้
-                      รพ.มีมาตรการป้องกันให้ผู้ป่วยไข้หวัด ตรวจนอกอาคารแล้วเพื่อลดความเสี่ยงของผู้รับบริการอื่นที่จำเป็นต้องมาใช้บริการในอาคาร ขอให้มั่นใจในมาตรการป้องกัน ขอบคุณค่ะ");
-    $usr_array = array("U0c98542ecd0bc1584a2ed29c8c7cc01d", "Ufd405ef2395db5dc8e2e4e724f5ae9a0");
-  
-$num = count($msg_array);
+    $conn = mysqli_connect($server,$suser,$spassword,$database);
+
+
+    if(mysqli_connect_error()){
+      echo "Connection Error. ".mysqli_connect_error();
+  }
+    mysqli_set_charset($conn, "utf8");
+
+    $strSQL = "SELECT DISTINCT userID FROM vaccine_app WHERE appoint_date > '2021-05-01' ";
+
+    $usr_array = array();
+    $result = mysqli_query($conn,$strSQL);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_array($result)) {
+            array_push($usr_array , $row["0"]);
+        }
+    } else {
+        echo "0 results";
+    }
+
+
+
+    $msg = "เนื่องจาก คำสั่งจากสาธารณสุขจังหวัดให้ รพ.เร่งฉีดวัคซีนล็อตนี้ให้หมด ก่อน 30 เม.ย.
+ดังนั้นวันที่ 2-7 พ.ค. จะไม่มีวัคซีนเข็ม1 ให้แล้ว
+
+ผู้จองวัคซีนในวันที่  2-7 พ.ค.ทุกคน  สามารถนำข้อความการจองเดิมด้านบน  มาขอรับวัคซีนที่ รพ.กระทุ่มแบน ในช่วงวันที่ 19-30 เม.ย.  เวลา 12.30-14.00 
+ยกเว้นวันเสาร์ที่ 24 เม.ย. รพ.หยุดให้บริการวัคซีน
+ขออภัยในความไม่สะดวกในครั้งนี้";
+
+$num = count($usr_array);
 
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
@@ -31,7 +58,7 @@ for($i=0;$i<$num;$i++){
 
 $pushID = $usr_array[$i];
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg_array[$i]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 echo $i." ";
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
